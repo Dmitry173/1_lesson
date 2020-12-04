a5f4988016ed7645b27e792e223693d4f202d40b
Dmitry173 <75272296+Dmitry173@users.noreply.github.com>
2020-12-04 22:10:41 +0300

    Add files via upload

--- size.rb
+++ size.rb
@@ -1,9 +1,9 @@
 puts "Как вас зовут?"
 name1 = gets.chomp
 puts "Какой у вас рост(просьба указать в см)?"
-height = gets.chomp
+height = gets.chomp.to_i
 
-weight = (height.to_i - 110)*1.15
+weight = (height - 110)*1.15
 
 if weight >= 0
   puts "#{name1}, Ваш идеальный вес равен #{weight}"
